d068965e289d60626c911fbcbdbebacb26986405
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-29 10:19:35 -0500

    🚀 [UPDATE] vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-router-course/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
